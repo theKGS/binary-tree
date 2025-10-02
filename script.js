8a4749901ea9922fcdee61c720be17be0a5d18d2
KGS <1698415+theKGS@users.noreply.github.com>
2025-10-02 17:13:01 +0200

    Implement buildTree function

---
+++ script.js
@@ -0,0 +1,61 @@
+class BinaryTree {
+    #root;
+
+    constructor(arr) {
+
+    }
+
+    static buildTree(array) {
+        // Set() converts array to set which removes duplicates
+        array = [...new Set(array)];
+        array.sort( (a, b) => {return a - b}); // to sort properly
+        console.log(array);
+        return BinaryTree.recBuild(array);
+    }
+
+    static recBuild(array) {
+        if (array.length === 0) {
+            return null
+        };
+
+        console.log("array " + array);
+
+        const midIdx = Math.floor(array.length / 2);
+        const midVal = array[midIdx];
+        array.splice(midIdx, 1);
+        
+        return new BNode(
+            BinaryTree.recBuild(array.slice(0, midIdx)),
+            BinaryTree.recBuild(array.slice(midIdx)),
+            midVal);
+    }
+}
+
+class BNode {
+    value;
+    right;
+    left;
+
+    constructor(left, right, val) {
+        this.right = right;
+        this.left = left;
+        this.value = val;
+    }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+
+// b = BinaryTree.buildTree([1, 2, 3, 4, 51, 12, 5, 6, 7]);
+// prettyPrint(b);
