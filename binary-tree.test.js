47d3275f0491a54a1ec8804b5610fe7132d07003
KGS <1698415+theKGS@users.noreply.github.com>
2025-10-02 22:13:41 +0200

    Add more tests for imbalanced trees

--- binary-tree.test.js
+++ binary-tree.test.js
@@ -1,13 +1,13 @@
 const BinaryTree = require('./binary-tree');
 
 test('test 1', () => {
-  let btree = new BinaryTree([1,1,1,2,3]);
+  let btree = new BinaryTree([1, 1, 1, 2, 3]);
   expect(btree.height(1)).toBe(0);
   expect(btree.depth(2)).toBe(0);
 });
 
 test('test height 1', () => {
-  let btree = new BinaryTree([1,2,3]);
+  let btree = new BinaryTree([1, 2, 3]);
   expect(btree.height(1)).toBe(0);
   expect(btree.height(2)).toBe(1);
   expect(btree.height(3)).toBe(0);
@@ -15,7 +15,7 @@ test('test height 1', () => {
 });
 
 test('test depth 1', () => {
-  let btree = new BinaryTree([1,2,3]);
+  let btree = new BinaryTree([1, 2, 3]);
   expect(btree.depth(1)).toBe(1);
   expect(btree.depth(2)).toBe(0);
   expect(btree.depth(3)).toBe(1);
@@ -23,10 +23,25 @@ test('test depth 1', () => {
 });
 
 test('test rebalance', () => {
-  let btree = new BinaryTree([1,2,3]);
+  let btree = new BinaryTree([1, 2, 3]);
   btree.rebalance();
   expect(btree.find(1)).not.toBeNull();
   expect(btree.find(2)).not.toBeNull();
   expect(btree.find(3)).not.toBeNull();
   expect(btree.values().length).toBe(3);
+});
+
+test('test imbalanced tree', () => {
+  let btree = new BinaryTree([1]);
+  btree.rawInsert(2);
+  btree.rawInsert(3);
+  btree.rawInsert(4);
+  btree.rawInsert(5);
+  expect(btree.height(1)).toBe(4);
+  expect(btree.height(5)).toBe(0);
+  expect(btree.depth(1)).toBe(0);
+  expect(btree.depth(2)).toBe(1);
+  expect(btree.depth(3)).toBe(2);
+  expect(btree.depth(4)).toBe(3);
+  expect(btree.depth(5)).toBe(4);
 });
